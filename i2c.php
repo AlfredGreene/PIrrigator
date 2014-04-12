f8a700584fac91dc5b226807617981c454bba762
micronen <micronen@gmail.com>
2014-04-12 20:28:14 +0300

    i2c - add 10 retries to read, cleanup errors

--- i2c.php
+++ i2c.php
@@ -1,14 +1,13 @@
 <?php
 class I2C
 {	
-	private $addr; 	// the i2c bus ( 0 on first gen rpi's, 1 on subsequnet rpi's ) 
-	                // and address of the unit being communicated with ( set when instantiated )
-
-	private $hwsim = null; // Simulate a device if no HW exists.
+	private $addr; 	 		// and address of the unit being communicated with ( set when instantiated )
+	private $hwsim = null; 	// Simulate a device if no HW exists.
+	const TRIES = 10;		// Number of tries before giving up
 	
-	function __construct($addr, $bus = 1) {		
+	function __construct($addr, $bus = 1) {	 // the i2c bus ( 0 on PI Ver. A, 1 on PI Ver B )	
 		$this->addr = $bus . ' ' . $addr;
-		if (!$this->hw_exists()) {
+		if (!$this->check_hw()) {
 			$this->hwsim = '/var/www-data/valves/i2c_hwsim' . $bus . '_' . $addr . '.txt';
 			$f = fopen($this->hwsim, 'r');
 			if ($f === false) {
@@ -20,7 +19,11 @@ class I2C
 	}
 
 	public function hw_exists() {
-		for ($i = 0; $i <= 10; $i++) {
+		return is_null($this->hwsim);
+	}
+		
+	private function check_hw() {
+		for ($i = 0; $i <= $this::TRIES; $i++) {
 			$ans = trim(shell_exec('/usr/sbin/i2cget -y ' . $this->addr . ' 2>&1'));
 			if (strcmp($ans, 'Error: Read failed')) {
 				return TRUE;
@@ -31,10 +34,17 @@ class I2C
 	
 	public function read_register($register = '') {
 		if (is_null($this->hwsim)) {
-//			echo '/usr/sbin/i2cget -y ' . $this->addr . ' ' . $register . PHP_EOL;
-			$ans = shell_exec('/usr/sbin/i2cget -y ' . $this->addr . ' ' . $register);
-//			echo '|' . trim($ans) . '|' . PHP_EOL;
-			return intval(trim($ans), 16);
+			for ($i = 0; $i <= $this::TRIES; $i++) {
+				$ans = trim(shell_exec('/usr/sbin/i2cget -y ' . $this->addr . ' ' . $register . ' 2>&1'));
+				if (strcmp($ans, 'Error: Read failed') != 0) {
+					if ($i > 0) {
+						error_log('Warning reading from ' . $this->addr . ' ' . $register . ' took ' . $i . 'tries' . PHP_EOL);
+					}
+					return intval($ans, 16);
+				}
+			}
+			error_log('Error reading from ' . $this->addr . ' ' . $register . PHP_EOL);
+			return 0;
 		} else {
 			$f = fopen($this->hwsim, 'r');
 			$value = intval(fgets($f));
@@ -45,18 +55,16 @@ class I2C
 			
 	public function write_register($value, $register = '') {
 		if (is_null($this->hwsim)) {
-//			echo '/usr/sbin/i2cset -y ' . $this->addr . ' ' . $register . ' ' . $value . PHP_EOL;
-			for ($i = 0; $i <= 10; $i++) {
-				shell_exec('/usr/sbin/i2cset -y ' . $this->addr . ' ' . $register . ' ' . $value );
+			for ($i = 0; $i <= $this::TRIES; $i++) {
+				shell_exec('/usr/sbin/i2cset -y ' . $this->addr . ' ' . $register . ' ' . $value . ' 2>&1' );
 				if ($this->read_register($register) == $value) {
-					break;
+					if ($i > 0) {
+						error_log('Warning writing value ' . $value . ' to ' . $this->addr . ' ' . $register . ' took ' . $i . 'tries' . PHP_EOL);
+					}
+					return;
 				}
 			}
-			if ($i >= 10) {
-				error_log('Error writing value ' . $value . ' read: ' . $this->read_register($register) . PHP_EOL);
-			} elseif ($i > 0) {
-				error_log('Warning value ' . $value . ' was written after ' . $i . ' tries.' . PHP_EOL);
-			}
+			error_log('Error writing value ' . $value . ' to ' . $this->addr . ' ' . $register . ' read: ' . $this->read_register($register) . PHP_EOL);
 		} else {
 			$f = fopen($this->hwsim, 'w');
 			fwrite($f, $value);
