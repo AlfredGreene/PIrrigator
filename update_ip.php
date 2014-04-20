567e6eee883f7a30f841572b6b3d370b9cc7e5dd
micronen <micronen@gmail.com>
2014-04-20 16:24:14 +0300

    Update IP code cleanup

--- update_ip.php
+++ update_ip.php
@@ -5,7 +5,7 @@ include_once 'valve.php';
 
 function GetExternalIP() {
 	$sGetIp = "http://ip4.me/";
-	$sAns = rtrim(implode(file($sGetIp), ""));
+	$sAns = implode(file($sGetIp));	
 	if (preg_match("#(\d+\.\d+\.\d+\.\d+)#i", $sAns, $aMatch)) 
 	{ 
 		$nIP = $aMatch[1]; 
@@ -19,6 +19,7 @@ function GetExternalIP() {
 	} else {
 		echo (new DateTime())->format(Valve::DATEFORMAT) . ": IP address not found in: $sAns" . PHP_EOL;
 	}	
+	return "";
 }
 
 function CheckIfIpChanged($sIniFilename) {
@@ -28,13 +29,11 @@ function CheckIfIpChanged($sIniFilename) {
 	
 	//print_r($params);
 	$IP = GetExternalIP();
-	if (!empty($IP)) {		
-		if ($IP != $params["IP"]["LastIP"]) {
-			echo (new DateTime())->format(Valve::DATEFORMAT) . ": IP Changed, External IP is: $IP, Last IP is: " . $params['IP']['LastIP'] . PHP_EOL;
-			$params["IP"]["LastIP"] = $IP;
-			ini_write($params, $sIniFilename, true);		
-			return true;
-		}
+	if ($IP != $params["IP"]["LastIP"]) {
+		echo (new DateTime())->format(Valve::DATEFORMAT) . ": IP Changed, External IP is: $IP, Last IP is: " . $params['IP']['LastIP'] . PHP_EOL;
+		$params["IP"]["LastIP"] = $IP;
+		ini_write($params, $sIniFilename, true);		
+		return true;
 	}
 	return false;
 }
@@ -45,6 +44,6 @@ function update_ip() {
 	if (CheckIfIpChanged(Valve::DEFAULTPATH . 'LastIP.ini')) {
 		$ans = file($sDnsUpdate);
 		$ans = rtrim(implode($ans, ""));
-		echo (new DateTime())->format(Valve::DATEFORMAT) . ': Dynamic DNS answer - ' . $ans . PHP_EOL;
+		echo (new DateTime())->format(Valve::DATEFORMAT) . ': Dynamic DNS answer: ' . $ans . PHP_EOL;
 	}
 }
